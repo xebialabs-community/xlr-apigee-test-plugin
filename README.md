11e20468ae23204dc5dd0ae9d59728ddec286800
Levent Tutar <ltutar@xebia.com>
2017-12-12 22:49:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Apigee Test plugin
+
+
+[![Build Status][xlr-apigee-test-plugin-travis-image]][xlr-apigee-test-plugin-travis-url]
+[![License: MIT][xlr-apigee-test-plugin-license-image]][xlr-apigee-test-plugin-license-url]
+![Github All Releases][xlr-apigee-test-plugin-downloads-image]
+
+[xlr-apigee-test-plugin-travis-image]: https://travis-ci.org/xebialabs-community/xlr-apigee-test-plugin.svg?branch=master
+[xlr-apigee-test-plugin-travis-url]: https://travis-ci.org/xebialabs-community/xlr-apigee-test-plugin
+[xlr-apigee-test-plugin-license-image]: https://img.shields.io/badge/License-MIT-yellow.svg
+[xlr-apigee-test-plugin-license-url]: https://opensource.org/licenses/MIT
+[xlr-apigee-test-plugin-downloads-image]: https://img.shields.io/github/downloads/xebialabs-community/xlr-apigee-test-plugin/total.svg
+
+## Preface
+
+This document describes the functionality provided by the XL Release Apigee plugin.
+
+See the [XL Release reference manual](https://docs.xebialabs.com/xl-release) for background information on XL Release and release orchestration concepts.  
+
+## Overview
+
+This XL Release plugin can compare a text string with the description field of an API Proxy that is deployed to an Apigee environment. 
+It is difficult to keep a track of the revisions that are deployed to different Apigee organizations. This is because of the fact that each Apigee organization repository comes up with its own revision numbers. The API Proxy helloworld can be revision 11 on Apigee organization ltutar-trial and be revision 2 on Apigee organization ltutar-eval. In order to overcome this problem, we inject our git tag version i.e. 1.2.3 to the description field of API Proxy Metadata before deploying to an Apigee environment. After deployment of the API Proxy, we can use this XLR plugin to compare the value of our git tag version with the content of the description field.
+
+![Screenshot of Apigee Metadata helloworld](images/metadata-description.jpg)
+
+
+## Requirements
+
+* XL Release 7.0+
+
+## Installation
+
+* Copy the latest JAR file from the [releases page](https://github.com/xebialabs-community/xlr-apigee-test-plugin/releases) into the `XL_RELEASE_SERVER/plugins` directory.
+* Restart the XL Release server.
+
+## Usage
+
+### Creating the shared configuration
+
+1. Go to `Settings` -> `Shared configuration`
+2. Click on `Add Server` of `Apigee:Server`
+3. Fill in the values and click on `Save` and `Test`. 
+   - If an HTTP Proxy is needed, please create one of type `Remote Script: HTTP proxy` from `Settings` -> `Shared configuration`
+
+### Choose Task
+
+1. Click on `Add Task` in the XLR template.
+2. Choose `Apigee:Compare Text With Description Field`
+3. Fill in the values.
+
+
+### Note: Apigee Server connection problems
+If there are problems connecting to the Apigee server, then you may get the following error
+```python
+requests.exceptions.SSLError: [Errno 1] Illegal state exception in <script> at line number 18
+```
+click on `retry` again.
+
+### Screenshots
+
+#### Screen shot of shared configuration
+![Screenshot of Shared configuration Apigee Server](images/apigee-server.jpg)
+
+#### Screen shot of Apigee task
+![Screenshot of XLR apigee task](images/xlr-apigee-task.jpg)
+
+#### Screen shot of equal content between text and description field
+![Screenshot of release with equal content](images/textfield_equal_descriptionfield.jpg)
+
+#### Screen shot of unequal content between text and description field
+![Screenshot of release with unequal content](images/textfield_unequal_descriptionfield.jpg)
---
+++ src/main/resources/apigee/ApigeeClient.py
@@ -0,0 +1,129 @@
+#
+# Copyright 2017 XebiaLabs, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+import sys
+import json
+import requests
+from requests.packages.urllib3.exceptions import SNIMissingWarning, InsecurePlatformWarning, InsecureRequestWarning
+import onetimepass as otp
+
+
+# The variable authorizationHeader has always the same value
+authorizationHeader = "ZWRnZWNsaTplZGdlY2xpc2VjcmV0"
+
+
+def setup_urllib():
+    requests.packages.urllib3.disable_warnings(SNIMissingWarning)
+    requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+class ApigeeClient(object):
+
+    def __init__(self, apigeeCI):
+        self.organizationName = apigeeCI['organizationName']
+        self.environmentName = apigeeCI['environmentName']
+        self.url = apigeeCI['url']
+        self.username = apigeeCI['username']
+        self.password = apigeeCI['password']
+        self.authentication = (apigeeCI['username'], apigeeCI['password'])
+        self.mfa = apigeeCI['mfa']
+        self.secretKey = apigeeCI['secretKey']
+        self.sso_login_url = "https://login.apigee.com/oauth/token"
+        self.proxy_dict = None
+        if apigeeCI['httpProxy']:
+            httpProxyAddress = apigeeCI['httpProxy'].getProperty('address')
+            httpProxyPort = apigeeCI['httpProxy'].getProperty('port')
+            self.proxy_dict = {httpProxyAddress + ":" + str(httpProxyPort)}
+            print("http proxy: %s \n" % (self.proxy_dict))
+
+    @staticmethod
+    def create_apigeeClient(apigeeCI):
+        return ApigeeClient(apigeeCI)
+
+    def get_revision_number_of_apiproxy_deployed_to_environment(self, apiProxyName, environmentName):
+        resp = self.build_url('/environments/' + environmentName + '/apis/' + apiProxyName + '/deployments')
+        jsonData = json.loads(resp.text)
+        revisionNumber = jsonData['revision'][0]['name']
+        print("Api proxy %s revision number %s is deployed to environment %s of Apigee organization %s \n" % (apiProxyName, revisionNumber, environmentName, self.organizationName))
+        return revisionNumber
+
+    def get_description_field_of_apiproxy_revision_number(self, apiProxyName, revisionNumber):
+        resp = self.build_url('/apis/' + apiProxyName + '/revisions/' + revisionNumber)
+        jsonData = json.loads(resp.text)
+        descriptionContent = jsonData['description']
+        print("Api proxy %s revision number %s has description field content %s \n" % (apiProxyName, revisionNumber, descriptionContent))
+        return descriptionContent
+
+    def compare_text_field_with_description_field(self, apiProxyName, environmentName, textField):
+        revisionNumber = self.get_revision_number_of_apiproxy_deployed_to_environment(apiProxyName, environmentName)
+        print("revisionNumber: " + revisionNumber + "\n")
+        descriptionField = self.get_description_field_of_apiproxy_revision_number(apiProxyName, revisionNumber)
+        print("descriptionField: " + descriptionField + "\n")
+        if (textField == descriptionField):
+            print("textField %s is equal to description field %s \n" % (textField, descriptionField))
+            return descriptionField
+        else:
+            print "textField %s is not equal to description field %s \n" % (textField, descriptionField)
+            sys.exit(1)
+
+    def get_environment_details(self):
+        resp = self.build_url('/environments/' + self.environmentName)
+        return resp
+
+    def create_one_time_password(self):
+        my_secret = self.secretKey
+        if my_secret is None:
+            raise Exception("Error during creating one time password. The secret key is empty. \n")
+        my_token = otp.get_totp(my_secret)
+        if len(str(my_token)) == 5:
+            my_token = "0%s" % my_token
+        return my_token
+
+    def build_authorization_header(self):
+        # Check the connection with an http head request. Otherwise, the password is printed when mfa is on.
+        resp = requests.head(self.sso_login_url, proxies=self.proxy_dict, verify=False)
+        authorization_headers = {}
+        if self.mfa:
+            my_token = self.create_one_time_password()
+            headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'Accept': 'application/json;charset=utf-8', 'Authorization': 'Basic ' + authorizationHeader}
+            params = {'username': self.username, 'password': self.password, 'grant_type': 'password', 'mfa_token': my_token}
+            resp = requests.post(self.sso_login_url, params=params, proxies=self.proxy_dict, verify=False, headers=headers)
+            if resp.status_code > 399:
+                print(resp.status_code)
+                print(resp.json())
+                raise Exception("Error during creating authorization header ", resp.json())
+            data = resp.json()
+            access_token = data['access_token']
+            authorization_headers = {'Authorization': 'Bearer ' + access_token}
+        return authorization_headers
+
+    def build_org_url(self):
+        base_url = self.url
+        url = base_url + "/v1/organizations/" + self.organizationName
+        return url
+
+    def build_url(self, contextRoot):
+        url = self.build_org_url() + contextRoot
+        authorization_headers = self.build_authorization_header()
+        print(url)
+        print("\n")
+        headers = authorization_headers
+        if self.mfa:
+            print("Multi factor authentication is on \n")
+            resp = requests.get(url, proxies=self.proxy_dict, verify=False, headers=headers)
+        else:
+            print("Multi factor authentication is off \n")
+            resp = requests.get(url, auth=self.authentication, proxies=self.proxy_dict, verify=False, headers=headers)
+        if resp.status_code > 399:
+            print(resp.status_code)
+            print(resp.json())
+            raise Exception("Error during getting deployment details \n", resp.json())
+        return resp
