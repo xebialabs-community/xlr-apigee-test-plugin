54c5ce7888fb8eb7701626575238d5d74cbe0b9f
Levent Tutar <ltutar@xebia.com>
2019-11-26 12:53:51 +0100

    timeout and better logging introduced

--- src/main/resources/apigee/ApigeeClient.py
+++ src/main/resources/apigee/ApigeeClient.py
@@ -8,6 +8,7 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
+import time
 import sys
 import json
 import requests
@@ -23,6 +24,24 @@ def setup_urllib():
     requests.packages.urllib3.disable_warnings(SNIMissingWarning)
     requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def print_response(response):
+    print("Response code: ")
+    print(response.status_code)
+    print("\n")
+    print("Response: ")
+    try:
+        print response.json()
+        print("\n")
+    except ValueError:
+        print("No JSON returned. \n")
+        print(response.content)
+        print("\n")
+
+def make_token_six_digits(token):
+    if len(str(token)) == 5:
+        token = "0%s" % token
+    return token
 
 
 class ApigeeClient(object):
@@ -43,54 +62,54 @@ class ApigeeClient(object):
             idx = httpProxyAddress.index(':')
             httpProxyPort = apigeeCI['httpProxy'].getProperty('port')
             self.proxy_dict = {httpProxyAddress[0:idx]: httpProxyAddress + ":" + str(httpProxyPort)}
-            print("http proxy: %s \n" % (self.proxy_dict))
+            print("Http proxy: %s \n" % self.proxy_dict)
 
     @staticmethod
     def create_apigeeClient(apigeeCI):
         return ApigeeClient(apigeeCI)
 
     def get_revision_number_of_apiproxy_deployed_to_environment(self, apiProxyName, apiType, environmentName):
-        if (apiType == "apiproxy"):
+        if apiType == "apiproxy":
             resp = self.build_url('/environments/' + environmentName + '/apis/' + apiProxyName + '/deployments')
-        elif (apiType == "sharedflow"):
+        elif apiType == "sharedflow":
             resp = self.build_url('/environments/' + environmentName + '/sharedflows/' + apiProxyName + '/deployments')
         else:
-            print "Api type %s is not valid. It should be apiproxy or sharedflow \n" % (apiType)
+            print "Api type %s is not valid. It should be apiproxy or sharedflow. \n" % apiType
             sys.exit(1)
         jsonData = json.loads(resp.text)
         lengthOfNames = len(jsonData['revision'])
-        if (lengthOfNames > 1):
+        if lengthOfNames > 1:
             print("There are multiple revisions of this %s %s deployed to environment %s \n" % (apiType, apiProxyName, environmentName))
             revisionNumber = jsonData['revision'][lengthOfNames -1]['name']
-            print("%s %s revision number %s is the highest revision deployed to environment %s of Apigee organization %s \n" % (apiType, apiProxyName, revisionNumber, environmentName, self.organizationName))
+            print("%s %s revision number %s is the highest revision deployed to environment %s of Apigee organization %s. \n" % (apiType.capitalize(), apiProxyName, revisionNumber, environmentName, self.organizationName))
         else:
             revisionNumber = jsonData['revision'][0]['name']
-            print("%s %s revision number %s is deployed to environment %s of Apigee organization %s \n" % (apiType, apiProxyName, revisionNumber, environmentName, self.organizationName)) 
+            print("%s %s revision number %s is deployed to environment %s of Apigee organization %s. \n" % (apiType.capitalize(), apiProxyName, revisionNumber, environmentName, self.organizationName)) 
         return revisionNumber
 
     def get_description_field_of_apiproxy_revision_number(self, apiProxyName, apiType, revisionNumber):
-        if (apiType == "apiproxy"):
+        if apiType == "apiproxy":
             resp = self.build_url('/apis/' + apiProxyName + '/revisions/' + revisionNumber)
-        elif (apiType == "sharedflow"):
+        elif apiType == "sharedflow":
             resp = self.build_url('/sharedflows/' + apiProxyName + '/revisions/' + revisionNumber)
         else:
-            print "Api type %s is not valid. It should be apiproxy or sharedflow \n" % (apiType)
+            print "Api type %s is not valid. It should be apiproxy or sharedflow. \n" % apiType
             sys.exit(1)
         jsonData = json.loads(resp.text)
         descriptionContent = jsonData['description']
-        print("%s %s revision number %s has description field content %s \n" % (apiType, apiProxyName, revisionNumber, descriptionContent))
+        print("%s %s revision number %s has description field content %s. \n" % (apiType.capitalize(), apiProxyName, revisionNumber, descriptionContent))
         return descriptionContent
 
     def compare_text_field_with_description_field(self, apiProxyName, environmentName, apiType, textField):
         revisionNumber = self.get_revision_number_of_apiproxy_deployed_to_environment(apiProxyName, apiType, environmentName)
-        print("revisionNumber: " + revisionNumber + "\n")
+        print("Revision number: " + revisionNumber + ".\n")
         descriptionField = self.get_description_field_of_apiproxy_revision_number(apiProxyName, apiType, revisionNumber)
-        print("descriptionField: " + descriptionField + "\n")
-        if (textField == descriptionField):
-            print("textField %s is equal to description field %s \n" % (textField, descriptionField))
+        print("Description field: " + descriptionField + "\n")
+        if textField == descriptionField:
+            print("Text field %s is equal to description field of deployed application %s. \n" % (textField, descriptionField))
             return descriptionField
         else:
-            print "textField %s is not equal to description field %s \n" % (textField, descriptionField)
+            print "Text field %s is NOT equal to description field of deployed application %s. \n" % (textField, descriptionField)
             sys.exit(1)
 
     def get_environment_details(self):
@@ -98,27 +117,39 @@ class ApigeeClient(object):
         return resp
 
     def create_one_time_password(self):
+        danger_time_interval_in_seconds = 7
         my_secret = self.secretKey
         if my_secret is None:
             raise Exception("Error during creating one time password. The secret key is empty. \n")
+        print("Creating the time-based token. \n")
         my_token = otp.get_totp(my_secret)
-        if len(str(my_token)) == 5:
-            my_token = "0%s" % my_token
-        return my_token
+        my_token = make_token_six_digits(my_token)
+        self.token = my_token
+        # Let's verify if the token is still valid after some seconds and we are not switching to new interval
+        my_token2 = otp.get_totp(my_secret, clock=(int(time.time()) + danger_time_interval_in_seconds))
+        my_token2 = make_token_six_digits(my_token2)
+        if my_token == my_token2:
+            return my_token
+        else:
+            print("Waiting for the next time interval ...\n")
+            time.sleep(danger_time_interval_in_seconds)
+            self.token = my_token2
+            return my_token2
 
     def build_authorization_header(self):
         # Check the connection with an http head request. Otherwise, the password is printed when mfa is on.
-        resp = requests.head(self.sso_login_url, proxies=self.proxy_dict, verify=False)
+        resp = requests.head(self.sso_login_url, proxies=self.proxy_dict, verify=False, timeout=60)
         authorization_headers = {}
         if self.mfa:
             my_token = self.create_one_time_password()
             headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', 'Accept': 'application/json;charset=utf-8', 'Authorization': 'Basic ' + authorizationHeader}
             params = {'username': self.username, 'password': self.password, 'grant_type': 'password', 'mfa_token': my_token}
-            resp = requests.post(self.sso_login_url, params=params, proxies=self.proxy_dict, verify=False, headers=headers)
-            if resp.status_code > 399:
-                print(resp.status_code)
-                print(resp.json())
-                raise Exception("Error during creating authorization header ", resp.json())
+            try:
+                resp = requests.post(self.sso_login_url, params=params, proxies=self.proxy_dict, verify=False, headers=headers, timeout=60)
+                resp.raise_for_status()
+            except requests.exceptions.HTTPError:
+                print_response(resp)
+                raise Exception("Error during creating authorization header.")
             data = resp.json()
             access_token = data['access_token']
             authorization_headers = {'Authorization': 'Bearer ' + access_token}
@@ -132,16 +163,17 @@ class ApigeeClient(object):
     def build_url(self, contextRoot):
         url = self.build_org_url() + contextRoot
         authorization_headers = self.build_authorization_header()
-        print("url: " + url + "\n")
+        print("Url: " + url + "\n")
         headers = authorization_headers
-        if self.mfa:
-            print("Multi factor authentication is on \n")
-            resp = requests.get(url, proxies=self.proxy_dict, verify=False, headers=headers)
-        else:
-            print("Multi factor authentication is off \n")
-            resp = requests.get(url, auth=self.authentication, proxies=self.proxy_dict, verify=False, headers=headers)
-        if resp.status_code > 399:
-            print(resp.status_code)
-            print(resp.json())
-            raise Exception("Error during getting deployment details \n", resp.json())
+        try:
+            if self.mfa:
+                print("Multi factor authentication is on. \n")
+                resp = requests.get(url, proxies=self.proxy_dict, verify=False, headers=headers, timeout=60)
+            else:
+                print("Multi factor authentication is off \n")
+                resp = requests.get(url, auth=self.authentication, proxies=self.proxy_dict, verify=False, headers=headers, timeout=60)
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print_response(resp)
+            raise Exception("Error during getting deployment details. \n")
         return resp
